1f862811e140c4de60822eb3e80e6e449dbad1b7
sandratra002 <ainarafanomazantsoa@gmail.com>
2024-02-26 19:49:59 +0300

    Testing signup

--- src/testing/Main.java
+++ src/testing/Main.java
@@ -7,7 +7,7 @@ public class Main {
     public static void main(String[] args) {
         try {
             // Userinfo.createUserinfo("Aina", "Sandratra", "aina@gmail.com", "aina");
-            Userinfo info = Authentication.login("aina@gmail.com", "aina1");
+            Userinfo info = Authentication.signup("Aina", "Sandratra", "aina@gmail.cmom", "aina", "aina");
             if(info != null){
                 System.out.println(info.getName());
             }else{
